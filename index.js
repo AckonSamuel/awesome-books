e9234887e52d7c136a9e00e02fa27ed7f89cc68c
unknown <saied2421998@gmail.com>
2022-02-14 21:00:51 +0200

    customize the JS file to depend only on arrays,objects&functions

--- index.js
+++ index.js
@@ -1,48 +1,42 @@
 /* eslint-disable linebreak-style */
-class BookList {
-  constructor() {
-    this.bookList = [];
-  }
+let bookList = [];
 
-  addBook(title = '', author = '') {
-    const list = document.querySelector('.booklist');
-    list.style.padding = '0';
-    const li = document.createElement('li');
-    li.style.margin = '0';
-    li.style.listStyleType = 'none';
-    const bookTitle = document.createElement('p');
-    bookTitle.className = 'title';
-    bookTitle.innerText = title;
-    const bookAuthor = document.createElement('p');
-    bookAuthor.className = 'author';
-    bookAuthor.innerText = author;
-    const removeBtn = document.createElement('button');
-    removeBtn.className = 'remove';
-    removeBtn.innerText = 'Remove';
-    const hr = document.createElement('hr');
-    hr.className = 'bottom-border';
-    li.appendChild(bookTitle);
-    li.appendChild(bookAuthor);
-    li.appendChild(removeBtn);
-    li.appendChild(hr);
-    list.appendChild(li);
-    const book = { title, author };
-    this.bookList.push(book);
-    window.localStorage.setItem('bookList', JSON.stringify(this.bookList));
-  }
+const addBook = (title = '', author = '') => {
+  const list = document.querySelector('.booklist');
+  list.style.padding = '0';
+  const li = document.createElement('li');
+  li.style.margin = '0';
+  li.style.listStyleType = 'none';
+  const bookTitle = document.createElement('p');
+  bookTitle.className = 'title';
+  bookTitle.innerText = title;
+  const bookAuthor = document.createElement('p');
+  bookAuthor.className = 'author';
+  bookAuthor.innerText = author;
+  const removeBtn = document.createElement('button');
+  removeBtn.className = 'remove';
+  removeBtn.innerText = 'Remove';
+  const hr = document.createElement('hr');
+  hr.className = 'bottom-border';
+  li.appendChild(bookTitle);
+  li.appendChild(bookAuthor);
+  li.appendChild(removeBtn);
+  li.appendChild(hr);
+  list.appendChild(li);
+  const book = { title, author };
+  bookList.push(book);
+  window.localStorage.setItem('bookList', JSON.stringify(bookList));
+};
 
-  removeBook(index) {
-    this.bookList = this.bookList.filter((book, i) => this.bookList[index] !== this.bookList[i]);
-    window.localStorage.setItem('bookList', JSON.stringify(this.bookList));
-  }
-}
-
-const listOfBooks = new BookList();
+const removeBook = (index) => {
+  bookList = bookList.filter((book, i) => bookList[index] !== bookList[i]);
+  window.localStorage.setItem('bookList', JSON.stringify(bookList));
+};
 
 const bookAdd = () => {
   const title = document.querySelector('#book_title').value;
   const author = document.querySelector('#book_author').value;
-  listOfBooks.addBook(title, author);
+  addBook(title, author);
 };
 
 const addBtn = document.querySelector('.add');
@@ -52,7 +46,7 @@ const bookRemove = (e) => {
   if (e.target.classList.contains('remove')) {
     const getLi = document.querySelectorAll('li');
     const index = Array.from(getLi).indexOf(e.target.parentNode);
-    listOfBooks.removeBook(index);
+    removeBook(index);
     document.querySelectorAll('li')[index].remove();
   }
 };
@@ -60,6 +54,6 @@ document.addEventListener('click', bookRemove);
 
 const showBookList = () => {
   const books = JSON.parse(window.localStorage.bookList);
-  books.forEach((book) => listOfBooks.addBook(book.title, book.author));
+  books.forEach((book) => addBook(book.title, book.author));
 };
-window.addEventListener('load', showBookList);
+showBookList();
