1c6e94cb44a61bdb2ed3c09c60dc18fa588c2a20
Samuel Ackon <ackons433@gmail.com>
2022-02-14 14:57:31 +0000

    Add remove button function

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
+/* eslint-disable linebreak-style */
 class BookList {
-	constructor (){
-		this.bookList=[]
-	}
-	add_book(title="",author=""){
+  constructor() {
+    this.bookList = [];
+  }
 
-      const list = document.querySelector('.booklist')
-      const li = document.createElement('li')
-      const bookTitle = document.createElement('p')
-      bookTitle.className = 'title'
-      bookTitle.innerText= title
-      const bookAuthor = document.createElement('p')
-      bookAuthor.className = 'author'
-      bookAuthor.innerText= author
-      const removeBtn = document.createElement('button')
-      removeBtn.className="remove"
-      removeBtn.innerText="Remove"
-      const hr = document.createElement('hr')
-      hr.className="bottom-border"
-      li.appendChild(bookTitle)
-      li.appendChild(bookAuthor)
-      li.appendChild(removeBtn)
-      li.appendChild(hr)
-      list.appendChild(li)
-      const book = {title,author}
-      this.bookList.push(book)
-      window.localStorage.setItem('bookList',this.bookList)
-	}
+  add_book(title = '', author = '') {
+    const list = document.querySelector('.booklist');
+    const li = document.createElement('li');
+    li.classList.add('show', 'delete');
+    const bookTitle = document.createElement('p');
+    bookTitle.className = 'title';
+    bookTitle.innerText = title;
+    const bookAuthor = document.createElement('p');
+    bookAuthor.className = 'author';
+    bookAuthor.innerText = author;
+    const removeBtn = document.createElement('button');
+    removeBtn.className = 'remove';
+    removeBtn.innerText = 'Remove';
+    const hr = document.createElement('hr');
+    hr.className = 'bottom-border';
+    li.appendChild(bookTitle);
+    li.appendChild(bookAuthor);
+    li.appendChild(removeBtn);
+    li.appendChild(hr);
+    list.appendChild(li);
+    const book = { title, author };
+    this.bookList.push(book);
+    window.localStorage.setItem('bookList', this.bookList);
+  }
+
+  remove_book(index) {
+
+      this.bookList = this.bookList.filter((book, i) => {
+
+              return this.bookList[index] !== this.bookList[i]
+        })
+  }
 }
 
-const listOfBooks = new BookList
-
-
+const listOfBooks = new BookList();
